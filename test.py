2f8169b5eddc4fa14b2944d34a757bd98223be8d
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-10-31 20:58:09 +0200

    Alignment checks; fixed a minor alignment issue by adding two new nested data types

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import time
 import pydsdl
 
-output = pydsdl.parse_namespace('uavcan', [])
+def on_print(definition, line, value):
+    print('%s:%d: %s' % (definition.file_path, line, value), file=sys.stderr)
+
+started_at = time.monotonic()
+
+output = pydsdl.parse_namespace('uavcan', [], print_directive_output_handler=on_print)
+
+elapsed_time = time.monotonic() - started_at
 
 print('\n'.join(map(str, output)))
+
+print('%d data types in %.1f seconds' % (len(output), elapsed_time), file=sys.stderr)
