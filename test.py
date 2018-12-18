3bc3b9dd3e6ee00b64b7fa2ea54d9acc7cda1c2d
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-12-18 14:16:58 +0200

    Richer output of the test script for diagnostics

--- test.py
+++ test.py
@@ -13,11 +13,36 @@ def on_print(definition, line, value):
           file=sys.stderr)
 
 
+def compute_max_num_frames_canfd(bit_length):
+    b = (bit_length + 7) // 8
+    if b <= 63:
+        return 1
+    else:
+        return (b + 2 + 62) // 63
+
+
 started_at = time.monotonic()
 output = pydsdl.parse_namespace('uavcan', [], print_directive_output_handler=on_print)
 elapsed_time = time.monotonic() - started_at
 
-print('\n'.join(map(str, output)))
+print('Full data type name'.center(58),
+      'FSID'.center(5),
+      'CAN FD fr'.center(9))
+
+for t in output:
+    num_frames_to_str = lambda x: str(x) if x > 1 else ' '      # Return empty for single-frame transfers
+    if isinstance(t, pydsdl.data_type.ServiceType):
+        max_canfd_frames = '  '.join([
+            num_frames_to_str(compute_max_num_frames_canfd(x.bit_length_range.max))
+            for x in (t.request_type, t.response_type)
+        ])
+    else:
+        max_canfd_frames = num_frames_to_str(compute_max_num_frames_canfd(t.bit_length_range.max))
+
+    print(str(t).ljust(58),
+          str(t.fixed_port_id if t.has_fixed_port_id else '').rjust(5),
+          max_canfd_frames.rjust(7) + ' ')
+
 print('%d data types in %.1f seconds' % (len(output), elapsed_time),
       file=sys.stderr)
 
